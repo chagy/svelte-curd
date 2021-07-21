3d4a09b4ee3592724f699bdd391253d1da4f0bfb
chagy <chagy_x_@hotmail.com>
2021-07-21 15:58:44 +0700

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,108 @@
 <script>
-	export let name;
+  let products = [
+    {
+      id: 1,
+      name: "HP Notebook",
+      description: "Hp Laptop",
+      category: "laptop",
+    },
+    {
+      id: 2,
+      name: "Mouse Razer",
+      description: "Gaming mouse",
+      category: "peripherials",
+    },
+  ];
+
+  export let product = {
+    id: "",
+    name: "",
+    description: "",
+    category: "",
+    imageURL: "",
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        {#each products as product}
+          <div class="card mt-2">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="" alt="" />
+              </div>
+              <div class="col-md-8">
+                <h5>
+                  <strong>{product.name}</strong>
+                  <span><small>{product.category}</small></span>
+                </h5>
+                <p class="card-text">{product.description}</p>
+                <button class="btn btn-danger">Delete</button>
+                <button class="btn btn-secondary">Edit</button>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <form on:submit={onSubmitHandler}>
+              <div class="form-group">
+                <input
+                  bind:value={product.name}
+                  class="form-control"
+                  type="text"
+                  placeholder="Product Name"
+                  id="product-name"
+                />
+              </div>
+              <div class="form-group">
+                <textarea
+                  bind:value={product.description}
+                  class="form-control"
+                  id="product-description"
+                  rows="3"
+                  placeholder="Product Description"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  bind:value={product.imageUrl}
+                  class="form-control"
+                  type="url"
+                  id="product-image-url"
+                  placeholder="URL"
+                />
+              </div>
+              <div class="form-group">
+                <select
+                  id="category"
+                  bind:value={product.category}
+                  class="form-control"
+                >
+                  <option value="laptops">Laptops</option>
+                  <option value="peripherials">Peripherials</option>
+                  <option value="servers">Servers</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <button class="btn btn-primary">Save</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
